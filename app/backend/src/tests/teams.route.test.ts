0b96a6b42daae7cbbe64ce4a342c871cdb9256ae
Fabricio Rodrigues <fabricio12nastacio@gmail.com>
2023-08-29 14:49:17 -0300

    Cria testes para a rota get/teams -req 2-

--- app/backend/src/tests/teams.route.test.ts
+++ app/backend/src/tests/teams.route.test.ts
@@ -1,10 +1,25 @@
-// import * as sinon from 'sinon';
-// import * as chai from 'chai';
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
 
-// import teamsModel from '../database/models'
+import { app } from '../app';
+import TeamModel from '../database/models/TeamModel';
+import mock from './mocks/teams.mock';
 
-// describe('Testando a rota /teams', function () {
-//   it('Verifca de se é possivel visualizar todos os times', async function () {
-//     sinon.stub(teamsModel, '')
-//   })
-// })
+const { mockTeams } = mock;
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teams test:', () => {
+  it('Verifica se é possivel retorna todos os times', async () => {
+    sinon.stub(TeamModel, 'findAll').resolves(mockTeams as any);
+
+    const { status, body } = await chai.request(app).get('/teams');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(mockTeams);
+  });
+});
